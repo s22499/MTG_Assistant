b61c03e3a00a1b16ebf55c257450fb5bd99be76c
Jakub Ślusarczyk <s26479@pjwstk.edu.pl>
2025-05-29 18:15:49 +0200

    Dodano serwis z odpytywaniem LLMa

---
+++ ai/src/services/llm_service.py
@@ -0,0 +1,20 @@
+import os
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from ai.src.prompts.prompt_templates import QUERY_REWRITE_PROMPT, ANSWER_PROMPT_TEMPLATE
+
+
+class LLMService:
+    def __init__(self):
+        self.api_key = os.environ["OPENAI_API_KEY_TEG"]
+        self.model = ChatOpenAI(api_key=self.api_key)
+
+    def refine_query(self, question):
+        prompt_template = ChatPromptTemplate.from_template(QUERY_REWRITE_PROMPT)
+        prompt = prompt_template.format(question=question)
+        return self.model.predict(prompt)
+
+    def generate_answer(self, context, question):
+        prompt_template = ChatPromptTemplate.from_template(ANSWER_PROMPT_TEMPLATE)
+        prompt = prompt_template.format(context=context, question=question)
+        return self.model.predict(prompt)
